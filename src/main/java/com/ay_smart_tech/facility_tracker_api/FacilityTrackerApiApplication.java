746a2332a1c736dfbc153f00f228a29840d88864
Abdulhafiz Yusuf <talk2abdulhafiz@gmail.com>
2026-07-08 10:16:29 +0100

    setup

---
+++ src/main/java/com/ay_smart_tech/facility_tracker_api/FacilityTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ay_smart_tech.facility_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ay_smart_tech/facility_tracker_api/FacilityTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ay_smart_tech.facility_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
